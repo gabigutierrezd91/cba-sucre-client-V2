3a000a3e4355a07b22f5e48e71509c7b108d1fd8
gabigutierrezd91 <gabi.gutierrezd91@gmail.com>
2020-03-09 07:40:14 -0400

    auth new

--- src/util/theme.js
+++ src/util/theme.js
@@ -1,45 +1,43 @@
 export default {
   palette: {
     primary: {
-      light: '#5471d2',
-      main: '#0d46a0',
-      dark: '#002071',
+      light: '#33c9dc',
+      main: '#00bcd4',
+      dark: '#008394',
       contrastText: '#fff'
     },
     secondary: {
-      light: '#5d6db8',
-      main: '#2a4288',
-      dark: '#001c5a',
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
       contrastText: '#fff'
     }
   },
-  spreadThis: {
-    typography: {
-      useNextVariants: true
-    },
-    form: {
-      textAlign: 'center'
-    },
-    image: {
-      margin: '20px auto 20px auto'
-    },
-    pageTitle: {
-      margin: '10px auto 10px auto'
-    },
-    textField: {
-      margin: '10px auto 10px auto'
-    },
-    button: {
-      marginTop: 20,
-      position: 'relative'
-    },
-    customError: {
-      color: 'red',
-      fontSize: '0.8rem',
-      marginTop: 10
-    },
-    progress: {
-      position: 'absolute'
-    }
+  typography: {
+    useNextVariants: true
+  },
+  form: {
+    textAlign: 'center'
+  },
+  image: {
+    margin: '20px auto 20px auto'
+  },
+  pageTitle: {
+    margin: '10px auto 10px auto'
+  },
+  textField: {
+    margin: '10px auto 10px auto'
+  },
+  button: {
+    marginTop: 20,
+    position: 'relative'
+  },
+  customError: {
+    color: 'red',
+    fontSize: '0.8rem',
+    marginTop: 10
+  },
+  progress: {
+    position: 'absolute'
   }
 };
